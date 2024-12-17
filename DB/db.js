2aebd1f464f355073ef2a1651ccdb6a2c30addf0
DevsFullStack2024 <devsfullstack2024@gmail.com>
2024-12-17 07:07:53 -0300

    Actualizar db.js

--- DB/db.js
+++ DB/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2'); // Importar la versión con promesas
+/*const mysql = require('mysql2'); // Importar la versión con promesas
 const config = require('../config/config')
 
 // Crear una conexión a la base de datos usando un pool para manejar múltiples conexiones
@@ -15,4 +15,27 @@ try {
     throw new Error('Error al crear la conexión a la base de datos');
 }
 }
-module.exports = db; // Exportar el pool de conexiones
+module.exports = db; // Exportar el pool de conexiones*/
+
+
+
+const mysql = require('mysql2/promise');
+const config = require('./config/config');
+
+const db = async () => {
+  try {
+    const pool = await mysql.createPool({
+      host: config.mysql.host,
+      user: config.mysql.user,
+      password: config.mysql.password,
+      database: config.mysql.database
+    });
+    console.log('Pool de conexiones a la base de datos creado');
+    return pool;
+  } catch (error) {
+    console.error('Error al crear el pool de conexiones:', error);
+    throw error;
+  }
+};
+
+module.exports = db;
