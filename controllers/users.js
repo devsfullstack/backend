26b494af5cf6e3913c1346d0e27d508eb74b3e96
Gregory <glrd4726ml@gmail.com>
2024-12-23 21:05:40 -0300

    Rutas de user funcionales getAll, getOne, Create, Update & Deleted

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const tabla = 'usuarios'
 
 const getAll = (req, res) => {
 
-    const sql = (`SELECT * FROM ${tabla}`)
+    const sql = (`SELECT * FROM ${tabla} ORDER BY created_at DESC`)
     db.query(sql, (err, results) => {
         if (err) {
             console.error(err.message);
@@ -21,10 +21,10 @@ const getAll = (req, res) => {
 
 
 const getOne = (req, res) => {
-    const {id, user, email, nombre} = req.body
+    const {id, usuario, email, nombre, cargo, rol} = req.body
     
     if(id > 0){
-        const sql = (`SELECT * FROM ${tabla} WHERE id = '${id}'`)
+        const sql = (`SELECT * FROM ${tabla} WHERE id_user = '${id}'`)
         db.query(sql, (err, results) => {
             if (err) {
                 console.error(err.message);
@@ -39,20 +39,21 @@ const getOne = (req, res) => {
     
         })
     })
-    }else if (user){
-        const sql = (`SELECT * FROM ${tabla} WHERE user = '${user}'`)
+    }else if (usuario){
+        const sql = (`SELECT * FROM ${tabla} WHERE usuario = '${usuario}'`)
         db.query(sql, (err, results) => {
             if (err) {
                 console.error(err.message);
                 return res.status(500).send(`Error al consultar la tabla: ${tabla}`);
                 }
                 if (results.length === 0) {
-                    return res.status(404).send(`El usuario '${user}' no existe`);
+                    return res.status(404).send(`El usuario '${usuario}' no existe`);
                     }
                     return res.status(200).json({
                         results
                     })
                 })
+
         }else if(email){
             const sql = (`SELECT * FROM ${tabla} WHERE email = '${email}'`)
             db.query(sql, (err, results) => {
@@ -82,7 +83,36 @@ const getOne = (req, res) => {
                             })
                             })
 
-        }
+        }else if(cargo){
+            const sql = (`SELECT * FROM ${tabla} WHERE cargo = '${cargo}'`)
+            db.query(sql, (err, results) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.status(500).send(`Error al consultar la tabla: ${tabla}`);
+                    }
+                    if (results.length === 0) {
+                        return res.status(404).send(`El usuario con cargo ${cargo} no existe`);
+                        }
+                        return res.status(200).json({
+                            results
+                            })
+                            })
+        }else if(rol){
+            const sql = (`SELECT * FROM ${tabla} WHERE rol = '${rol}'`)
+            db.query(sql, (err, results) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.status(500).send(`Error al consultar la tabla: ${tabla}`);
+                    }
+                    if (results.length === 0) {
+                        return res.status(404).send(`El usuario con rol ${rol} no existe`);
+                        }
+                        return res.status(200).json({
+                            results
+                            })
+                            })
+                        }
+                    
 }
 
 
@@ -90,22 +120,59 @@ const create = (req, res) => {
 
     const { nombre, apellido, usuario, contraseña, email, cargo, rol } = req.body;
 
+    if(!nombre || !apellido || !usuario || !contraseña || !email || !cargo || !rol){
+        return res.status(400).send('Faltan campos por completar')
+        }
+    
+
 
     const password2 = bcrypt.hashSync(contraseña, 10);
  
     const sql = (`INSERT INTO ${tabla} (nombre, apellido, usuario, contraseña, email, cargo, rol) VALUES ("${nombre}", "${apellido}", "${usuario}", "${password2}", "${email}", "${cargo}","${rol}")`)
     db.query(sql, (err, results) => {
-        if (err) { return res.status(500).send(`Error creando registro en tabla: ${tabla}`) }
+        if (err) { 
+            return res.status(500).send(`Error creando registro en tabla: ${tabla}`) 
+        }else{
             
-        return res.status(200).json(results)});
-
+        return res.status(200).json(results)
         }
+    });
+
+}
 
 
 
-    const update = (req, res)=>{}    
+    const update = (req, res)=>{
+        const {id, nombre, apellido, usuario, contraseña, email, cargo, rol} = req.body;
 
-    const deleted = (req,res) =>{}
+        const hashedPassword = bcrypt.hashSync(contraseña, 10);
+
+        const sql = (`UPDATE ${tabla} SET nombre = '${nombre}', apellido = '${apellido}', usuario = '${usuario}', contraseña = '${hashedPassword}', email = '${email}', cargo = '${cargo }', rol = '${rol}' WHERE id_user = '${id}'`)
+        db.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).send(`Error actualizando registro en tabla: ${tabla}`)
+                }
+                if (results.affectedRows === 0) {
+                    return res.status(404).send(`El usuario con id '${id}' no existe`)
+                    }
+                    return res.status(200).json(results)
+                    })
+    }    
+
+    const deleted = (req,res) =>{
+        const {id} = req.body;
+
+        const sql = (`DELETE FROM ${tabla} WHERE id_user = '${id}'`)
+        db.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).send(`Error eliminando registro en tabla: ${tabla}`)
+                }
+                if (results.affectedRows === 0) {
+                    return res.status(404).send(`El usuario con id ${id} no existe`)
+                    }
+                    return res.status(200).json(results)
+                    })
+    }
     
 module.exports = {
     getOne,
--- routes/categorias.js
+++ routes/categorias.js
@@ -4,7 +4,6 @@ const controllers = require('../controllers/categorias')
 const path = require('path')
 
 router.get('/categorias', controllers.getAll)
-router.get(path.join('/'+'?:id'), controllers.getOne)
 router.post('/categorias', controllers.create)
 router.put('/categorias', controllers.update)
 router.delete('/categorias', controllers.deleted)
